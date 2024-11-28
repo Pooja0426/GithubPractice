0cb7d40cd1fff0f7bac73f49a34352b6cff9c020
arun.laskar <thakare.rohini96@gmail.com>
2024-11-28 13:27:24 +0530

    Demo3 class

--- src/Sample/Demo1.java
+++ src/Sample/Demo1.java
@@ -6,6 +6,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("First program");
+		
 	}
 
 }
---
+++ src/Sample/Demo3.java
@@ -0,0 +1,12 @@
+package Sample;
+
+public class Demo3 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hii Pooja kshi aahes");
+
+	}
+
+}
